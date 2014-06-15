e3f254e15d6d69b727b3a824aeca5914249012d3
Ziggurat <thesurgingsplurge@gmail.com>
2014-06-15 17:20:30 +0100

    Changed controller to return Map instead of multimap

--- src/main/java/shortinterest/web/ShortPositionsController.java
+++ src/main/java/shortinterest/web/ShortPositionsController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/feed")
@@ -20,9 +22,9 @@ public class ShortPositionsController {
     private FcaSpreadsheetScraper fcaSpreadsheetScraper;
 
     @RequestMapping(value = "/shortpositions", method = RequestMethod.GET)
-    public Multimap<String, ShortPosition> shortPositions() {
+    public Map<String, Collection<ShortPosition>> shortPositions() {
         try {
-            return fcaSpreadsheetScraper.getShortPositions(new URL("http://www.fca.org.uk/static/documents/short-positions-daily-update.xls"), 0);
+            return fcaSpreadsheetScraper.getShortPositions(new URL("http://www.fca.org.uk/static/documents/short-positions-daily-update.xls"), 0).asMap();
         } catch (MalformedURLException e) {
             throw new IllegalStateException(e);
         }
